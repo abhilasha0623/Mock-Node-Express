164f2a291e2ab2f31316611594585d1d77cbfaf1
abhilasha.92.kp@gmail.com <abhilasha.92.kp@gmail.com>
2023-09-21 20:24:12 -0300

    changes on the mock api

--- app.js
+++ app.js
@@ -54,3 +54,54 @@ app.get('/foodList', (request, response) => {
         }]
     response.send(foodInfo);
 });
+
+
+app.get('/employeeDetails',(request, response) => {
+const employeeDetails = [
+    {
+   "Name": "Jeff",
+   "Zip": "55422",
+   "Age": 54,
+   "Pay": 9587.23,
+   "DepartmentId": 1,
+   "GetTaxForm": "1099"
+   },
+   
+   {
+   "Name": "Dave",
+   "Zip": "03456",
+   "Age": 41,
+   "Pay": 8547.55,
+   "DepartmentId": 1,
+   "GetTaxForm": "W2"
+   },
+   
+   {
+   "Name": "Amber",
+   "Zip": "41908",
+   "Age": 35,
+   "Pay": 4878.1,
+   "DepartmentId": 2,
+   "GetTaxForm": "W2"
+   },
+   
+   {
+   "Name": "Cassie",
+   "Zip": "91820",
+   "Age": 28,
+   "Pay": 4500,
+   "DepartmentId": 1,
+   "GetTaxForm": "1099"
+   },
+   
+   {
+   "Name": "Albert",
+   "Zip": "54321",
+   "Age": 39,
+   "Pay": 5874.09,
+   "DepartmentId": 2,
+   "GetTaxForm": "1099"
+   }
+   ]
+   response.send(employeeDetails);
+})
